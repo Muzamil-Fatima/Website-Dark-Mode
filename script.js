5d6d9f1992a603667298fcf5861704481cb71a58
Muzamil <muzamilzubair987654@gmail.com>
2025-06-21 19:59:42 +0500

    Remove error

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var icon = document.getElementById("icon");
 icon.onclick = function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        icon.src = "images\sun.png";
+        icon.src = "sun.png";
     }
     else{
         icon.src = "moon.png";
